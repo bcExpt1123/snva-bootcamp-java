ce5a15cb3b401931da4d1d5765967b50daf81861
bcExpt1123 <bcExpt1123@gmail.com>
2023-12-18 15:30:15 -0500

    Update mediaplayer

--- src/mediaplayer/Audio.java
+++ src/mediaplayer/Audio.java
@@ -6,20 +6,24 @@ public class Audio implements Media {
     @Override
     public void play() {
         // Play audio logic
+        System.out.println("Play audio.");
     }
 
     @Override
     public void pause() {
         // Pause audio logic
+        System.out.println("Pause Audio.");
     }
 
     @Override
     public void stop() {
         // Stop audio logic
+        System.out.println("Stop Audio.");
     }
 
     @Override
     public void seek(int position) {
         // Seek audio logic
+        System.out.println("Seek Audio " + position);
     }
 }
--- src/mediaplayer/Video.java
+++ src/mediaplayer/Video.java
@@ -6,20 +6,24 @@ public class Video implements Media {
     @Override
     public void play() {
         // Play video logic
+        System.out.println("Pause Video.");
     }
 
     @Override
     public void pause() {
         // Pause video logic
+        System.out.println("Pause Video.");
     }
 
     @Override
     public void stop() {
         // Stop video logic
+        System.out.println("Stop Video.");
     }
 
     @Override
     public void seek(int position) {
         // Seek video logic
+        System.out.println("Seek Video.");
     }
 }
